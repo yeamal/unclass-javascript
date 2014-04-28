2f2f7691c6dbaf993352c6455194cf05db4756cf
Mallory Busch <M.MalloryBusch@gmail.com>
2014-04-28 14:09:26 -0500

    JS VI

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
+	$('#Thumbnails').click(function(){
+		$('#primary').fadeTo("fast", 0.5);
+	});
 
+	$('#Thumbnails2').click(function(){
+		$('#primary').attr("src","cat-two.jpg");
+	});
+
+	$('#Thumbnails3').click(function(){
+		$('#primary').attr("src","cat-three.jpg");
+	});
+
+	$('#Thumbnails4').click(function(){
+		$('#primary').attr("src","cat-four.jpg");
+	});
 });
